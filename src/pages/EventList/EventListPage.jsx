e31134af1df0299634e01b8cd45106285f08a928
Lenis Cervantes <leniserik@gmail.com>
2022-10-04 10:51:40 +0200

    spinner

--- src/pages/EventList/EventListPage.jsx
+++ src/pages/EventList/EventListPage.jsx
@@ -1,14 +1,16 @@
 import { useState, useEffect } from 'react';
+import { Spinner } from 'react-bootstrap';
 import EventAxios from "../../services/eventAxios";
 import EventList from '../../components/EventList/EventList.jsx'
 import SearchBar from '../../components/Searchbar/Searchbar'
 
 function EventListPage() {
-    const [events, setevents] = useState([]);
-    const _events = [...events]
+    const [events, setevents] = useState(null);
+    const [_events, setcopy] = useState(null);
 
 
     const callEventAxios = new EventAxios()
+
     const findEvents = () => {
         callEventAxios
             .getAllEvents()
@@ -22,13 +24,23 @@ function EventListPage() {
 
     useEffect(() => {
         findEvents()
+        console.log(events)
+        setcopy(events)
+        console.log(_events)
     }, []);
 
-
-    return (<>
-        <SearchBar events={_events} setevents={setevents} />
-        <EventList events={events} setevents={setevents} />
-    </>
+    if (!events) {
+        return (
+            <Spinner animation='border' role='status'>
+                <span className='visually-hidden'>Loading...</span>
+            </Spinner>
+        )
+    }
+    else return (
+        <>
+            <SearchBar events={_events} setevents={setevents} />
+            <EventList events={events} setevents={setevents} />
+        </>
     )
 }
 export default EventListPage;
