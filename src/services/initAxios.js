5e4db7da8b6f4a23d5007a514ceeca0e36cc4b21
Lenis Cervantes <leniserik@gmail.com>
2022-10-11 14:09:16 +0200

    errata

--- src/services/initAxios.js
+++ src/services/initAxios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class InitAxios {
     constructor(path) {
         this.axios = axios.create({
-            baseURL: `https://lets-go-ride.herokuapp.com//${path}`
+            baseURL: `https://lets-go-ride.herokuapp.com/${path}`
         })
     }
 }
